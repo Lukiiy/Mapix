2cde6a2e6e3d31089703e7161d7a419ae8e7af6a
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2026-06-23 14:47:55 -0300

    Add SessionManager

---
+++ src/main/java/me/lukiiy/mapix/SessionManager.java
@@ -0,0 +1,8 @@
+package me.lukiiy.mapix;
+
+import net.kyori.adventure.text.format.TextColor;
+
+public class SessionManager {
+    private static final TextColor FIRST_POSITION = TextColor.color(0x7874b7);
+    private static final TextColor SECOND_POSITION = TextColor.color(0xaf8a6f);
+}
