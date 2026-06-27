119d1521769df2af69327d11682f95d9fb62b2ad
Lukiiy <lukiiy@proton.me>
2026-06-26 22:53:09 -0300

    Init sesionManager & worldManager

--- src/main/java/me/lukiiy/mapix/Mapix.java
+++ src/main/java/me/lukiiy/mapix/Mapix.java
@@ -1,12 +1,14 @@
 package me.lukiiy.mapix;
 
 import me.lukiiy.mapling.WorldManager;
+import me.lukiiy.mapling.provided.TomlWorldDataStore;
+import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Mapix extends JavaPlugin {
-    private WorldManager<World> worldManager;
-    private SessionManager sessionManager;
+    private final WorldManager<World> worldManager = new WorldManager<>(Bukkit.getWorldContainer(), new BukkitWorldAdapter(), new TomlWorldDataStore());
+    private final SessionManager sessionManager = new SessionManager();
 
     @Override
     public void onEnable() {
