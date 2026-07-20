7c2ded198bee7cf31da8403d8c0e29fbbade69e6
Lukiiy <lukiiy@proton.me>
2026-07-20 09:21:26 -0300

    Add Location to String util function

--- src/main/java/me/lukiiy/mapix/Utils.java
+++ src/main/java/me/lukiiy/mapix/Utils.java
@@ -4,6 +4,8 @@ import me.lukiiy.mapling.Position;
 import org.bukkit.Location;
 import org.bukkit.World;
 
+import java.util.Locale;
+
 public class Utils {
     public static Position toPos(Location loc) {
         return new Position(loc.getX(), loc.getY(), loc.getZ(), loc.getYaw(), loc.getPitch());
@@ -11,5 +13,11 @@ public class Utils {
 
     public static Location toLoc(World world, Position pos) {
         return new Location(world, pos.getX(), pos.getY(), pos.getZ(), pos.getYaw(), pos.getPitch());
+    }
+
+    public static String toString(Location location) {
+        if (location == null) return "Not set!";
+
+        return String.format(Locale.US, "%.2f %.2f %.2f : %.2f %.2f", location.x(), location.y(), location.z(), location.getYaw(), location.getPitch());
     }
 }
