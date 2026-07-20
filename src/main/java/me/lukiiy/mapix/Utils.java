e7eab90fd5945e629eda52063720792a86861b30
Lukiiy <lukiiy@proton.me>
2026-07-20 11:22:26 -0300

    Use DecimalFormat in Utils#toString

--- src/main/java/me/lukiiy/mapix/Utils.java
+++ src/main/java/me/lukiiy/mapix/Utils.java
@@ -4,9 +4,13 @@ import me.lukiiy.mapling.Position;
 import org.bukkit.Location;
 import org.bukkit.World;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.Locale;
 
 public class Utils {
+    private static final DecimalFormat DECIMAL_FORMATTER = new DecimalFormat("#.##", DecimalFormatSymbols.getInstance(Locale.US));
+
     public static Position toPos(Location loc) {
         return new Position(loc.getX(), loc.getY(), loc.getZ(), loc.getYaw(), loc.getPitch());
     }
@@ -18,6 +22,6 @@ public class Utils {
     public static String toString(Location location) {
         if (location == null) return "Not set!";
 
-        return String.format(Locale.US, "%.2f %.2f %.2f : %.2f %.2f", location.x(), location.y(), location.z(), location.getYaw(), location.getPitch());
+        return String.format("%s %s %s : %s %s", DECIMAL_FORMATTER.format(location.x()), DECIMAL_FORMATTER.format(location.y()), DECIMAL_FORMATTER.format(location.z()), DECIMAL_FORMATTER.format(location.getYaw()), DECIMAL_FORMATTER.format(location.getPitch()));
     }
 }
